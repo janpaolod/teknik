1b6bf14b4e8632d3db34ea3f9635123cfa6a812e
jp <janpaolod@gmail.com>
2012-04-03 10:56:12 +0800

    add google analytic code

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -84,5 +84,15 @@ $(function (){
     $('.countdown').each(function(){
       $(this).countdown({until: $(this).text()});
     });
+    
+      var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-XXXXX-X']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
 });
 
